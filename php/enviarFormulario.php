10b3301b0217188244788ca08d1c75998bc32f5f
BMDanilo <34260544+BMDanilo@users.noreply.github.com>
2021-07-30 00:48:32 -0300

    update

--- php/enviarFormulario.php
+++ php/enviarFormulario.php
@@ -1,5 +1,12 @@
 <?php
 header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+header("Allow: GET, POST, OPTIONS, PUT, DELETE");
+$method = $_SERVER['REQUEST_METHOD'];
+if($method == "OPTIONS") {
+    die();
+}
 
         if (!empty($_SERVER['HTTP_X_REQUESTED_WITH'])){
           // Recibir vía POST los datos del formulario
