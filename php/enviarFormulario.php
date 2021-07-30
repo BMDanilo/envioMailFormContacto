7b7f0f5236f0ea2fd6fe9040b77ce3ddd65385d0
BMDanilo <34260544+BMDanilo@users.noreply.github.com>
2021-07-30 00:32:41 -0300

    Update enviarFormulario.php

--- php/enviarFormulario.php
+++ php/enviarFormulario.php
@@ -1,22 +1,10 @@
 <?php
 // Allow from any origin
-if (isset($_SERVER['HTTP_ORIGIN'])) {
-  header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
-  header('Access-Control-Allow-Credentials: true');
-  header('Access-Control-Max-Age: 86400');    // cache for 1 day
-}
-
-// Access-Control headers are received during OPTIONS requests
-if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
-
-  if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
-      header("Access-Control-Allow-Methods: GET, POST, OPTIONS");         
-
-  if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
-      header("Access-Control-Allow-Headers:        {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
-
-  exit(0);
-}
+header('Access-Control-Allow-Origin: https://bmdanilo.github.io');
+header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
+header('Access-Control-Allow-Credentials: true');
+header('Access-Control-Max-Age:86400');
+header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token,  Accept, Authorization, X-Requested-With');
 
         if (!empty($_SERVER['HTTP_X_REQUESTED_WITH'])){
           // Recibir vía POST los datos del formulario
